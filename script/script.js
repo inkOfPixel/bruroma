1f529462cda4c803e15dbca1de116d859155e6de
mmarcon91 <m.marcon.91@gmail.com>
2015-09-09 14:16:48 +0200

    working on media query (problem center class wrapper)

--- script/script.js
+++ script/script.js
@@ -21,10 +21,12 @@ $(document).scroll(function() {
 // resize video header
 $(window).resize(function() {
     resizeBackgroundVideo();
+    paddingWrapper();
 });
 
 $(document).ready(function() {
     resizeBackgroundVideo();
+    paddingWrapper();
 });
 
 function resizeBackgroundVideo() {
@@ -45,3 +47,35 @@ function resizeBackgroundVideo() {
     videoWrapper.css("margin-left", -(videoWrapper.width()/2 - wvideo/2) );
 }
 
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function (event) {
+    var that = $(this);
+
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none");
+}
+
+var onMapClickHandler = function (event) {
+    var that = $(this);
+
+    // Disable the click handler until the user leaves the map area
+    that.off('click', onMapClickHandler);
+
+    // Enable scrolling zoom
+    that.find('iframe').css("pointer-events", "auto");
+
+    // Handle the mouse leave event
+    that.on('mouseleave', onMapMouseleaveHandler);
+}
+
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.maps.embed-container').on('click', onMapClickHandler);
+
+
+function paddingWrapper() {
+    var wwrapper = $('.wrapper').width();
+    var wrapper = $(".wrapper");
+
+    wrapper.css("padding-left", -(wwrapper/2 );
+}
