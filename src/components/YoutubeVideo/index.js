bd6a96589cee94e3a3cd3082e395a8cbf48436ba
macs <massimodemarchi91@gmail.com>
2018-05-20 13:23:20 +0200

    fixed types

--- src/components/YoutubeVideo/index.js
+++ src/components/YoutubeVideo/index.js
@@ -3,40 +3,44 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
-class YoutubeVideo extends Component {
-	render() {
-		return (
-			<Wrapper>
-				<InnerWrapper>
-					<Iframe src={this.props.src} />
-				</InnerWrapper>
-			</Wrapper>
-		);
-	}
+type Props = {
+  src: string
+};
+
+class YoutubeVideo extends Component<Props> {
+  render() {
+    return (
+      <Wrapper>
+        <InnerWrapper>
+          <Iframe src={this.props.src} />
+        </InnerWrapper>
+      </Wrapper>
+    );
+  }
 }
 
 const Wrapper = styled.div`
-	background-color: black;
-	height: 600px;
-	left: 0;
-	overflow: hidden;
-	position: fixed;
-	top: 0;
-	width: 100%;
+  background-color: black;
+  height: 600px;
+  left: 0;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  width: 100%;
 `;
 
 const InnerWrapper = styled.div`
-	height: 800px;
-	margin-top: -100px;
-	position: relative;
+  height: 800px;
+  margin-top: -100px;
+  position: relative;
 `;
 
 const Iframe = styled.iframe`
-	height: 100%;
-	left: 0;
-	position: absolute;
-	top: 0;
-	width: 100%;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
 `;
 
 export default YoutubeVideo;
